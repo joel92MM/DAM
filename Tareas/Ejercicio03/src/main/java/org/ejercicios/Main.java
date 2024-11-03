b172c76eff31082afaf4a360380f1c72783a8129
joel92MM <joel_tf_92@hotmail.com>
2024-11-03 13:48:02 +0000

    Ejercicio 03 Se hace la primera opcion del menu de la tarea

--- Tareas/Ejercicio03/src/main/java/org/ejercicios/Main.java
+++ Tareas/Ejercicio03/src/main/java/org/ejercicios/Main.java
@@ -2,11 +2,82 @@ package org.ejercicios;
 
 import org.ejercicios.clases.ConexionJDBC;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static ConexionJDBC conexion;
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        ConexionJDBC conexion = new ConexionJDBC();
-        conexion.conectorJDBC();
+        mostrarMenu();
+    }
+
+    public static void mostrarMenu() {
+        configurarConexion();
+        int opcion;
+        do {
+            System.out.println("\nSeleccione una opción:");
+            System.out.println("1. Mostrar información general de la base de datos");
+            System.out.println("2. Mostrar información de la tabla pasajeros");
+            System.out.println("3. Ver información de pasajeros de un vuelo");
+            System.out.println("4. Insertar un vuelo");
+            System.out.println("5. Borrar un vuelo");
+            System.out.println("6. Modificar vuelos de fumadores a no fumadores");
+            System.out.println("0. Salir");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1 -> mostrarInformacionDB();
+               // case 2 -> mostrarTablaPasajeros();
+                //  case 3 -> verPasajerosDeVuelo();
+               // case 4 -> insertarVuelo();
+               // case 5 -> borrarVuelo();
+               // case 6 -> modificarVuelosNoFumadores();
+                case 0 -> System.out.println("Saliendo...");
+                default -> System.out.println("Opción no válida, intente de nuevo.");
+            }
+        } while (opcion != 0);
+
+        scanner.close();
+    }
+
+    private static void configurarConexion() {
+        //String urljdbc = "jdbc:mysql://localhost:3306/aerolinea";
+        conexion = new ConexionJDBC();
+
+        String urljdbc = "jdbc:mariadb://localhost:3306/aerolinea";
+        String usuario = "root";
+        String contrasena = "";
+
+        conexion.detallesConexion(urljdbc, usuario, contrasena);
+    }
+    private static void mostrarInformacionDB() {
+        try {
+            conexion.abrirConexion();
+            DatabaseMetaData dbmd = conexion.getConexion().getMetaData();
+
+            //Obtener la información de la base de datos
+            String nombreDB = dbmd.getDatabaseProductName();
+            String nombreVersion = dbmd.getDatabaseProductVersion();
+            String nombreDriver = dbmd.getDriverName();
+            String nombreDriverVersion = dbmd.getDriverVersion();
+            String nombreURL = dbmd.getURL();
+            String usuarioDB = dbmd.getUserName();
+
+            System.out.println("Nombre de la base de datos: " + nombreDB);
+            System.out.println("Versión de la base de datos: " + nombreVersion);
+            System.out.println("Nombre del driver: " + nombreDriver);
+            System.out.println("Versión del driver: " + nombreDriverVersion);
+            System.out.println("URL de la base de datos: " + nombreURL);
+            System.out.println("Usuario de la base de datos: " + usuarioDB);
+
+            conexion.cerrarConexion();
+        } catch (SQLException | RuntimeException e) {
+            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+        }
     }
 }
--- Tareas/Ejercicio03/src/main/java/org/ejercicios/clases/ConexionJDBC.java
+++ Tareas/Ejercicio03/src/main/java/org/ejercicios/clases/ConexionJDBC.java
@@ -3,38 +3,70 @@ package org.ejercicios.clases;
 import java.sql.*;
 
 public class ConexionJDBC {
+    private Connection conexion;
+    private String urljdbc;
+    private String usuario;
+    private String contrasena;
 //    public void conectorJDBC(String urljdbc, String usuario, String contrasena) {
-public void conectorJDBC() {
+    public void conectorJDBC() {
 
-    Connection conexion = null;
-        try {
-            // Establecemos la conexión con la BD de MariaDB
-            String urljdbc = "jdbc:mariadb://localhost:3306/aerolinea"; // Cambia "nombre_base_datos" al nombre de tu BD
-            String usuario = "root";
-            String contrasena = "";
+        Connection conexion = null;
+            try {
+                // Establecemos la conexión con la BD de MariaDB
+                String urljdbc = "jdbc:mariadb://localhost:3306/aerolinea"; // Cambia "nombre_base_datos" al nombre de tu BD
+                String usuario = "root";
+                String contrasena = "";
 
-            // Conexión con MariaDB
-            conexion = DriverManager.getConnection(urljdbc, usuario, contrasena);
-            System.out.println("Conexión exitosa a MariaDB!");
-            Statement sentencia = conexion.createStatement();
+                // Conexión con MariaDB
+                conexion = DriverManager.getConnection(urljdbc, usuario, contrasena);
+                System.out.println("Conexión exitosa a MariaDB!");
+                Statement sentencia = conexion.createStatement();
 
-            String sql = "SELECT COD_VUELO, DESTINO, PROCEDENCIA FROM VUELOS";
-            ResultSet resultado = sentencia.executeQuery(sql);
+                String sql = "SELECT COD_VUELO, DESTINO, PROCEDENCIA FROM VUELOS";
+                ResultSet resultado = sentencia.executeQuery(sql);
 
-            // Procesar y mostrar resultados
-            while (resultado.next()) {
-                String id = resultado.getString("COD_VUELO");           // Cambia el nombre de columna si es necesario
-                String destino = resultado.getString("DESTINO");
-                String procedencia = resultado.getString("PROCEDENCIA");
+                // Procesar y mostrar resultados
+                while (resultado.next()) {
+                    String id = resultado.getString("COD_VUELO");           // Cambia el nombre de columna si es necesario
+                    String destino = resultado.getString("DESTINO");
+                    String procedencia = resultado.getString("PROCEDENCIA");
 
-                System.out.println("COD_VUELO: " + id + ", Destino: " + destino + ", Procedencia: " + procedencia);
+                    System.out.println("COD_VUELO: " + id + ", Destino: " + destino + ", Procedencia: " + procedencia);
+                }
+
+                // Cerrar conexión
+                conexion.close();
+            } catch (SQLException cn) {
+                cn.printStackTrace();
             }
 
-            // Cerrar conexión
-            conexion.close();
-        } catch (SQLException cn) {
-            cn.printStackTrace();
         }
 
+    public void detallesConexion(String urljdbc, String usuario, String contrasena){
+        this.urljdbc = urljdbc;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
     }
+    public void abrirConexion() throws SQLException {
+        if (conexion == null || conexion.isClosed()) {
+            conexion = DriverManager.getConnection(urljdbc, usuario, contrasena);
+            System.out.println("Conexión exitosa a la base de datos establecida!");
+        }
+    }
+    public void cerrarConexion() throws SQLException {
+        if (conexion!= null && !conexion.isClosed()) {
+            conexion.close();
+            System.out.println("Conexión cerrada con exito!");
+        }
+    }
+    public ResultSet ejecutarConsulta(String sql) throws SQLException {
+        abrirConexion();
+        Statement sentencia = conexion.createStatement();
+        return sentencia.executeQuery(sql);
+    }
+    public Connection getConexion() {
+        return conexion;
+    }
+
 }
+
