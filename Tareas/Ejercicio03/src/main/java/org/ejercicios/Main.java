3a201670bad0f8ccbdc29776dc70fa1812a7ed36
joel92MM <joel_tf_92@hotmail.com>
2024-11-04 13:31:30 +0000

    Ejercicio 03 Se finaliza el ejercicio

--- Tareas/Ejercicio03/src/main/java/org/ejercicios/Main.java
+++ Tareas/Ejercicio03/src/main/java/org/ejercicios/Main.java
@@ -1,11 +1,12 @@
 package org.ejercicios;
 
+import com.toedter.calendar.JDateChooser;
 import org.ejercicios.clases.ConexionJDBC;
 
 import javax.swing.*;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -37,8 +38,8 @@ public class Main {
                 case 2 -> mostrarTablaPasajeros();
                 case 3 -> verPasajeroDeVuelo();
                 case 4 -> insertarVuelo();
-               // case 5 -> borrarVuelo();
-               // case 6 -> modificarVuelosNoFumadores();
+                case 5 -> borrarVuelo();
+                case 6 -> modificarVuelosNoFumadores();
                 case 0 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción no válida, intente de nuevo.");
             }
@@ -48,10 +49,11 @@ public class Main {
     }
 
     private static void configurarConexion() {
-        //String urljdbc = "jdbc:mysql://localhost:3306/aerolinea";
+        String urljdbc = "jdbc:mysql://localhost:3306/aerolinea";
         conexion = new ConexionJDBC();
 
-        String urljdbc = "jdbc:mariadb://localhost:3306/aerolinea";
+        //En mi caso, no puedo instalar MYSQL, por lo qu tengo que usar la base de datos mariadb
+        //String urljdbc = "jdbc:mariadb://localhost:3306/aerolinea";
         String usuario = "root";
         String contrasena = "";
 
@@ -98,7 +100,7 @@ public class Main {
     private static void verPasajeroDeVuelo()  {
         try {
             conexion.abrirConexion();
-            ResultSet consulta = conexion.ejecutarConsulta("SELECT * FROM PASAJEROS WHERE num =138 AND cod_vuelo = 'SP-DC-438'");
+            ResultSet consulta = conexion.ejecutarConsulta("SELECT * FROM PASAJEROS WHERE cod_vuelo = 'SP-DC-438'");
             while (consulta.next()) {
                 System.out.println("Pasajero número" + consulta.getString("num") + ", codigo de vuelo "
                         +consulta.getString("cod_vuelo") + ", tipo de plaza " + consulta.getString("tipo_plaza") + ", fumador: " + consulta.getString("fumador") + ".\n");
@@ -109,28 +111,89 @@ public class Main {
     }
 
     private static void insertarVuelo()  {
-
-
         try {
-            String num= JOptionPane.showInputDialog("Ingrese el número del pasajero");
-
+            conexion.abrirConexion();
 
             String codVuelo = JOptionPane.showInputDialog("Ingrese el código de vuelo");
 
+            String destino = JOptionPane.showInputDialog("Ingrese el destino del vuelo");
+            String procedencia = JOptionPane.showInputDialog("Ingrese la procedencia del vuelo");
+            int numero_plaza_fumadores = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de plazas fumadores"));
+            int numero_plaza_nofumadores = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de plazas de no fumadores"));
+            int plazas_turistas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de plazas turistas (TU)"));
+            int plazas_primera_clase = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de plazas de la primera clase (PR)"));
 
-            String tipoPlaza = JOptionPane.showInputDialog("Ingrese el tipo de plaza (ej. PR(Premium) o TR(Turista)");
+            // Configurar el selector de fecha
+            JDateChooser dateChooser = new JDateChooser();
+            dateChooser.setDateFormatString("yyyy-MM-dd");
+            JOptionPane.showMessageDialog(null, dateChooser, "Seleccione la fecha de salida", JOptionPane.PLAIN_MESSAGE);
+            Date fechaSeleccionada = dateChooser.getDate();
 
-            String esFumador = JOptionPane.showInputDialog("¿Es fumador? (si/no): ");
+            // Captura de la hora en formato de texto
+            String horaStr = JOptionPane.showInputDialog("Ingrese la hora de salida (formato HH:mm:ss):");
 
-            String fumador = esFumador.toUpperCase().trim();
-            conexion.abrirConexion();
-            conexion.ejecutarConsulta("INSERT INTO PASAJEROS VALUES ('" + num + "', '" + codVuelo +
-                    "', '" + tipoPlaza + "', '" + fumador + "')");
-            System.out.println("Vuelo insertado con éxito.");
+            // Combinar fecha y hora
+            String fechaHoraStr = new SimpleDateFormat("yyyy-MM-dd").format(fechaSeleccionada) + " " + horaStr;
+            Timestamp fechaHora = Timestamp.valueOf(fechaHoraStr);
 
+            String sql="INSERT INTO VUELOS VALUES ('" + codVuelo + "', '" + fechaHoraStr + "', '" + destino.toUpperCase().trim() + "', '" + procedencia.toUpperCase().trim()
+                    + "', '" + numero_plaza_fumadores  + "', '" + numero_plaza_nofumadores + "', '" + plazas_turistas + "', '" + plazas_primera_clase + "')";
+
+            PreparedStatement consultaInsert = conexion.getConexion().prepareStatement(sql);
+            consultaInsert.setString(1, codVuelo);
+            consultaInsert.setString(2, fechaHoraStr);
+            consultaInsert.setString(3, destino);
+            consultaInsert.setString(4, procedencia);
+            consultaInsert.setInt(5, numero_plaza_fumadores);
+            consultaInsert.setInt(6, numero_plaza_nofumadores);
+            consultaInsert.setInt(7, plazas_turistas);
+
+            int filasInsertadas = consultaInsert.executeUpdate();
+
+            if (filasInsertadas > 0) {
+                System.out.println("Vuelo insertado correctamente.");
+            } else {
+                System.out.println("Error al insertar el vuelo.");
+            }
+            conexion.cerrarConexion();
         } catch (SQLException | RuntimeException e) {
             System.out.println("Error al conectar con la base de datos: " + e.getMessage());
         }
 
+    }
+    private static void borrarVuelo()  {
+        try {
+            conexion.abrirConexion();
+            String codVuelo = JOptionPane.showInputDialog("Ingrese el código de vuelo");
+            String sql="DELETE FROM VUELOS WHERE cod_vuelo = '" + codVuelo + "'";
+            PreparedStatement consultaInsert = conexion.getConexion().prepareStatement(sql);
+            int filasInsertadas = consultaInsert.executeUpdate();
+
+            if (filasInsertadas > 0) {
+                System.out.println("Vuelo borrado correctamente.");
+            } else {
+                System.out.println("Error al borrar el vuelo.");
+            }
+            conexion.cerrarConexion();
+        } catch (SQLException | RuntimeException e) {
+            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+        }
+    }
+    private static void modificarVuelosNoFumadores()  {
+        try {
+            conexion.abrirConexion();
+            String sql="UPDATE PASAJEROS SET FUMADOR = 'NO'";
+            PreparedStatement consultaInsert = conexion.getConexion().prepareStatement(sql);
+            int filasInsertadas = consultaInsert.executeUpdate();
+
+            if (filasInsertadas > 0) {
+                System.out.println("Vuelos modificados correctamente.");
+            } else {
+                System.out.println("Error al modificar el vuelo.");
+            }
+            conexion.cerrarConexion();
+        } catch (SQLException | RuntimeException e) {
+            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+        }
     }
 }
